863383f217aa992b50c033fea93644febf377795
Jahid458 <Jahidul898@outlook.com>
2025-01-14 23:35:14 +0600

    get payment history payment email

--- index.js
+++ index.js
@@ -224,6 +224,15 @@ async function run() {
       })
     });
 
+      app.get('/payments/:email',verifyToken, async(req,res) =>{
+        const query = {email: req.params.email}
+        if(req.params.email !== req.decoded.email){
+           return res.status(403).send({message: 'forbiddn access'})
+        }
+         const result = await paymentCollections.find(query).toArray();
+         res.send(result)
+      })
+
     //payment related APIS
     app.post('/payments', async(req,res) =>{
       const payment = req.body;
